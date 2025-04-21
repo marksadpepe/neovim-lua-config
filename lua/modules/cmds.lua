e010acf96f49d10951f414ba45d2c4946c177399
Mykhailo Voznesenskyi <mykhailo.voznesenskyi@amedia.ua>
2025-04-21 15:59:14 +0300

    autocommand to open in new Tab from quick fix window

--- lua/modules/cmds.lua
+++ lua/modules/cmds.lua
@@ -1,8 +1,14 @@
 vim.cmd('filetype plugin indent on')
-vim.cmd('colorscheme gruvbox')
+vim.cmd('colorscheme OceanicNext')
 vim.cmd([[
   hi DiagnosticError guifg=Red
   hi DiagnosticWarn  guifg=White
   hi DiagnosticInfo  guifg=White
   hi DiagnosticHint  guifg=White
 ]])
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "qf",
+  callback = function()
+    vim.api.nvim_buf_set_keymap(0, 'n', 'T', '<C-W><CR><C-W>T', { noremap = true })
+  end
+})
